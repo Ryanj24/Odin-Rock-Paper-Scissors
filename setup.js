1d84076b4df7a8117338a54227758e1cf8b8b08e
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-02-09 09:26:32 +0000

    Changes to styling and layout of setup page

---
+++ setup.js
@@ -0,0 +1,5 @@
+// Get the elements from the html document
+const lowerRound = document.querySelector("#decrease");
+const increaseRound = document.querySelector("#increase");
+const roundNumber = document.querySelector("#rounds");
+const btn = document.querySelector("#btn");
