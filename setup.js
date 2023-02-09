331d6f222383378bb5b93b8788afe1d68e5f237b
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-02-09 09:53:29 +0000

    Added some logic to the increase and decrease buttons on setup page

--- setup.js
+++ setup.js
@@ -1,5 +1,30 @@
 // Get the elements from the html document
 const lowerRound = document.querySelector("#decrease");
 const increaseRound = document.querySelector("#increase");
-const roundNumber = document.querySelector("#rounds");
-const btn = document.querySelector("#btn");
+const roundNumber = document.querySelector("#rounds").textContent;
+const submitBtn = document.querySelector("#btn");
+const myForm = document.querySelector(".roundSelection");
+let counter = 0;
+
+
+// Event listeners to change the number displayed on screen
+
+lowerRound.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    if (counter > 0) {
+        counter--;
+    }
+    
+    console.log(counter);
+})
+
+increaseRound.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    if (counter < 10) {
+        counter++;
+    }
+    
+    console.log(counter);
+})
