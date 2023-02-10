53f27f3a994a8d9a23da0ce6f95214e22d5372f7
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-02-10 16:58:57 +0000

    Corrected mistakes with files and reverted to a single end page

--- userWinEndPage.js
+++
@@ -1,6 +0,0 @@
-const user = parseInt(localStorage.getItem("final-user-score"));
-
-const computer = parseInt(localStorage.getItem("final-comp-score"));
-
-console.log(user);
-console.log(computer);
