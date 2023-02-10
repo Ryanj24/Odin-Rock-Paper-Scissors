58768af6c417378ba73279981e033ed8caf1c17e
Ryan Jackson <57965902+Ryanj24@users.noreply.github.com>
2023-02-10 09:48:54 +0000

    Completed styling for game page but logic not functioning correctly

--- game.js
+++ game.js
@@ -1,4 +1,36 @@
-/* Play a single round of rock, paper, scissors passing both the user and computer choices 
+// Need to change the round back to number as localstorage converts to string
+const roundsToPlay = parseInt(localStorage.getItem("rounds"));
+//console.log(typeof(rounds));
+
+let roundNumber = document.querySelector("#round-count");
+let playerScore = document.querySelector("#player-score-value");
+let computerScore = document.querySelector("#computer-score-value");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const roundWinner = document.querySelector("#round-winner-text");
+let playerChoice = "";
+
+let playerScoreCounter = 0;
+let computerScoreCounter = 0;
+
+rock.addEventListener("click", () => {
+    playerChoice = "rock";
+    //console.log(playerChoice);
+})
+
+paper.addEventListener("click", () => {
+    playerChoice = "paper";
+    //console.log(playerChoice);
+})
+
+scissors.addEventListener("click", () => {
+    playerChoice = "scissors";
+    //console.log(playerChoice);
+})
+
+
+// Play a single round of rock, paper, scissors passing both the user and computer choices 
 // to the function
 function playRound(playerChoice, computerChoice) {
 
@@ -7,23 +39,23 @@ function playRound(playerChoice, computerChoice) {
 
     // All different possible outcomes from the round
     if (playerChoice === "rock" && computerChoice === "rock") {
-        winner = `Tie! ${playerChoice} doesn't beat ${computerChoice}`;
+        winner = "tie";
     } else if (playerChoice === "rock" && computerChoice === "paper") {
-        winner = `You Lose! ${computerChoice} beats ${playerChoice}`;
+        winner = "computer";
     } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        winner = `You Win! ${playerChoice} beats ${computerChoice}`;
+        winner = "player";
     } else if (playerChoice === "paper" && computerChoice === "rock") {
-        winner = `You Win! ${playerChoice} beats ${computerChoice}`;
+        winner = "player";
     } else if (playerChoice === "paper" && computerChoice === "paper") {
-        winner = `Tie! ${playerChoice} doesn't beat ${computerChoice}`;
+        winner = "tie";
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
         winner = `You Lose! ${computerChoice} beats ${playerChoice}`;
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
         winner = `You Lose! ${computerChoice} beats ${playerChoice}`;
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        winner = `You Win! ${playerChoice} beats ${computerChoice}`;
+        winner = "player";
     } else if (playerChoice === "scissors" && computerChoice === "scissors") {
-        winner = `Tie! ${playerChoice} doesn't beat ${computerChoice}`;
+        winner = "tie";
     }
 
     // Return the round winner
@@ -34,49 +66,32 @@ function game() {
     // Possible choices for the user and computer to select from
     possibleChoices = ["rock", "paper", "scissors"];
 
+
     // Initialise variables to keep track of scores
-    let playerScore = 0, computerScore = 0;
-    let overallWinner = "";
+    let playerScoreCounter = 0;
+    let computerScoreCounter = 0;
 
-    // Loop for 5 rounds of rock, paper, scissors
-    for (i = 0; i < 5; i++) {
+    // Loop for specified number of rounds of rock, paper, scissors
+    for (i = 0; i < roundsToPlay; i++) {
         // Player and Computer choices
-        let playerChoice = prompt("Rock, paper or sicssors?").toLowerCase();
         const computerChoice = possibleChoices[Math.floor(Math.random() * 3)];
-
-        // Check to ensure user inputs a valid choice
-        while (!possibleChoices.includes(playerChoice)) {
-            playerChoice = prompt("Rock, paper or sicssors?").toLowerCase();
-        }
 
         // Simulate the round, storing the result in roundWinner variable
         const roundWinner = playRound(playerChoice, computerChoice);
 
         // Updating the scores depending on who won the round
-        if (roundWinner === `You Win! ${playerChoice} beats ${computerChoice}`) {
-            playerScore++;
-        } else if (roundWinner === `You Lose! ${computerChoice} beats ${playerChoice}`) {
-            computerScore++;
+        if (roundWinner === "player") {
+            playerScoreCounter++;
+            playerScore.textContent = playerScoreCounter;
+            roundWinner.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
+        } else if (roundWinner === "computer") {
+            computerScoreCounter++;
+            computerScore.textContent = computerScoreCounter;
+            roundWinner.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
+        } else {
+            roundWinner.textContent = `Tie! ${playerChoice} draws with ${computerChoice}`;
         }
     }
-
-    // Checks for the final scores to determine the overall winner
-    if (playerScore === computerScore) {
-        overallWinner = `Tie! \n Player Score: ${playerScore} \n Computer Score: ${computerScore}`;
-    } else if (playerScore > computerScore) {
-        overallWinner = `You Win! \n Player Score: ${playerScore} \n Computer Score: ${computerScore}`;
-    } else {
-        overallWinner = `You Lose! \n Player Score: ${playerScore} \n Computer Score: ${computerScore}`;
-    }
-
-    // Return the overall winner of the game
-    return overallWinner;
 }
 
-console.log(game());*/
-
-
-
-// Need to change the round back to number as localstorage converts to string
-const rounds = parseInt(localStorage.getItem("rounds"));
-console.log(typeof(rounds));
+game();
